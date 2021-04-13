d252b14e77c16cac006555c7bce316edbbc441c5
soekarnohatta <hidahhusni@gmail.com>
2021-04-14 05:52:40 +0700

    Add initial metrics
    Signed-off-by: soekarnohatta <hidahhusni@gmail.com>

---
+++ bot/models/chat.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"context"
+	"github.com/soekarnohatta/Siskamling/bot/helpers/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Chat struct {
+	ChatID    int64   `json:"chat_id" bson:"chat_id" `
+	ChatType  string `json:"chat_type" bson:"chat_type" `
+	ChatLink  string `json:"chat_link" bson:"chat_link" `
+	ChatTitle string `json:"chat_title" bson:"chat_title" `
+}
+
+func GetChatByID(ctx context.Context, Id int) (*Chat, error) {
+	var chat Chat
+	dat, err := database.Mongo.Collection("chat").FindOne(ctx, bson.M{"chat_id": Id}).DecodeBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	err = bson.Unmarshal(dat, chat)
+	return &chat, err
+}
+
+func SaveChat(ctx context.Context, chat Chat) error {
+	_, err:= database.Mongo.Collection("chat").UpdateOne(ctx, bson.M{"chat_id": chat.ChatID}, bson.D{{"$set", chat}}, options.Update().SetUpsert(true))
+	return err
+}
+
+func DeleteChatByID(ctx context.Context, Id int) error {
+	_, err := database.Mongo.Collection("chat").DeleteOne(ctx, bson.M{"chat_id": Id})
+	return err
+}
+
---
+++ bot/models/user.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"context"
+	"github.com/soekarnohatta/Siskamling/bot/helpers/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type User struct {
+	UserID    int64  `json:"user_id" bson:"user_id" `
+	FirstName string `json:"user_first_name" bson:"user_first_name" `
+	LastName  string `json:"user_last_name" bson:"user_last_name" `
+	UserName  string `json:"user_username" bson:"user_username" `
+}
+
+func GetUserByID(ctx context.Context, Id int) (*User, error) {
+	var user User
+	dat, err := database.Mongo.Collection("user").FindOne(ctx, bson.M{"user_id": Id}).DecodeBytes()
+	if err != nil {
+		return nil, err
+	}
+
+	err = bson.Unmarshal(dat, user)
+	return &user, err
+}
+
+func SaveUser(ctx context.Context, user User) error {
+	_, err:= database.Mongo.Collection("user").UpdateOne(ctx, bson.M{"user_id": user.UserID}, bson.D{{"$set", user}}, options.Update().SetUpsert(true))
+	return err
+}
+
+func DeleteUserByID(ctx context.Context, Id int) error {
+	_, err := database.Mongo.Collection("user").DeleteOne(ctx, bson.M{"user_id": Id})
+	return err
+}
+
