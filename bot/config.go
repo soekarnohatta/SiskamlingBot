33c4fec73edc6f17bde5aa42f6ce59c884d0f773
soekarnohatta <hidahhusni@gmail.com>
2021-04-14 05:51:59 +0700

    Get config from os if there is no conf file
    Signed-off-by: soekarnohatta <hidahhusni@gmail.com>

--- bot/config.go
+++ bot/config.go
@@ -3,6 +3,9 @@ package bot
 import (
 	"github.com/caarlos0/env"
 	"github.com/joho/godotenv"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type TgConfig struct {
@@ -19,7 +22,7 @@ type TgConfig struct {
 	SudoUsers     []int  `env:"SUDO_USERS,required" envSeparator:":"`
 	LogEvent      int64  `env:"LOG_EVENT,required"`
 	LogBan        int64  `env:"LOG_BAN,required"`
-	MainGrp       int64  `env:"MAIN_GRP,required"`
+	MainGroup     int64  `env:"MAIN_GRP,required"`
 	IsDebug       bool   `env:"IS_DEBUG"`
 	CleanPolling  bool   `env:"CLEAN_POLLING,required"`
 }
@@ -31,15 +34,45 @@ func NewConfig() TgConfig {
 
 	err := godotenv.Load("data/.env")
 	if err != nil {
-		panic(err.Error())
+		returnConfig.WebhookURL = os.Getenv("WEBHOOK_URL")
+		returnConfig.WebhookPath = os.Getenv("WEBHOOK_PATH")
+		returnConfig.WebhookPort, _ = strconv.Atoi(os.Getenv("WEBHOOK_PORT"))
+		returnConfig.WebhookServe = os.Getenv("WEBHOOK_SERVE")
+
+		returnConfig.BotAPIKey = os.Getenv("BOT_API_KEY")
+		_, cleanPolling := os.LookupEnv("CLEAN_POLLING")
+		returnConfig.CleanPolling = cleanPolling
+		_, isDebug := os.LookupEnv("IS_DEBUG")
+		returnConfig.IsDebug = isDebug
+
+		logEvent, _ := strconv.Atoi(os.Getenv("LOG_EVENT"))
+		returnConfig.LogEvent = int64(logEvent)
+		logBan, _ := strconv.Atoi(os.Getenv("LOG_BAN"))
+		returnConfig.LogBan = int64(logBan)
+		returnConfig.OwnerID, _ = strconv.Atoi(os.Getenv("OWNER_ID"))
+		returnConfig.SudoUsers = strToIntSlice(strings.Split(os.Getenv("SUDO_USERS"), ","))
+
+		returnConfig.DatabaseURL = os.Getenv("DATABASE_URL")
+		returnConfig.RedisAddress = os.Getenv("REDIS_ADDRESS")
+		returnConfig.RedisPassword = os.Getenv("REDIS_PASSWORD")
+
+		return returnConfig
 	}
 
-	errParse := env.Parse(&returnConfig)
-	if errParse != nil {
-		panic(errParse)
+	err = env.Parse(&returnConfig)
+	if err != nil {
+		panic(err.Error())
 	}
 
 	Config = returnConfig
 	return returnConfig
 }
 
+func strToIntSlice(s []string) []int {
+	var newIntSlice []int
+	for _, val := range s {
+		newInt, _ := strconv.Atoi(val)
+		newIntSlice = append(newIntSlice, newInt)
+	}
+	return newIntSlice
+}
