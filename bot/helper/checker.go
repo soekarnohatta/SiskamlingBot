648229206038a90360672b57336bc7cba80591bc
soekarnohatta <hidahhusni@gmail.com>
2021-06-04 09:47:40 +0700

    Fix checker logic temporarily
    - this fix is used until a new one has been discovered
    
    Signed-off-by: soekarnohatta <hidahhusni@gmail.com>

--- bot/helper/checker.go
+++ bot/helper/checker.go
@@ -8,19 +8,33 @@ import (
 )
 
 func IsUserBotRestricted(ctx *telegram.TgContext, app *app.MyApp) bool {
-	getUsername, _ := model.GetUsernameByID(app.DB, context.TODO(), ctx.Message.From.Id)
-	if getUsername != nil && getUsername.ChatID == ctx.Message.Chat.Id && getUsername.IsMuted {
-		if !IsUserRestricted(ctx) {return false}
+	if IsUsernameRestricted(ctx, app) {
+		if !IsUserRestricted(ctx) {
+			return false
+		} else if IsProfileRestricted(ctx, app) {
+			return true
+		}
 		return true
-	}
-
-	getPicture, _ := model.GetPictureByID(app.DB, context.TODO(), ctx.User.Id)
-	if getPicture != nil && getPicture.ChatID == ctx.Chat.Id && getPicture.IsMuted {
-		if !IsUserRestricted(ctx) {return false}
+	} else if IsProfileRestricted(ctx, app) {
+		if !IsUserRestricted(ctx) {
+			return false
+		} else if IsUsernameRestricted(ctx, app) {
+			return true
+		}
 		return true
 	}
 
 	return IsUserRestricted(ctx)
+}
+
+func IsUsernameRestricted(ctx *telegram.TgContext, app *app.MyApp) bool {
+	getUsername, _ := model.GetUsernameByID(app.DB, context.TODO(), ctx.Message.From.Id)
+	return getUsername != nil && getUsername.ChatID == ctx.Message.Chat.Id && getUsername.IsMuted
+}
+
+func IsProfileRestricted(ctx *telegram.TgContext, app *app.MyApp) bool {
+	getPicture, _ := model.GetPictureByID(app.DB, context.TODO(), ctx.User.Id)
+	return getPicture != nil && getPicture.ChatID == ctx.Chat.Id && getPicture.IsMuted
 }
 
 func IsUserRestricted(ctx *telegram.TgContext) bool {
